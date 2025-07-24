8735966acaa0eb74911a1764fbc190b8c94ef6ff
Reinaldo <reinaldo18junior@gmail.com>
2025-07-24 15:42:12 -0300

    criando model Category e ligando ForeignKey

--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 
 # blank -> opcional, não obrigatório
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -12,6 +19,9 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
-
-    def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+        )
