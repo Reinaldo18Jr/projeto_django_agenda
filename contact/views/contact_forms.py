7d5ebe55f98e2cc768a1c1fc13d9a2d410a2937c
Reinaldo <reinaldo18junior@gmail.com>
2025-07-31 15:10:54 -0300

    fazendo url, view e template para criar novo contato

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+from django.db.models import Q
+
+from contact.models import Contact
+
+def create(request):
+    context = {
+
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
