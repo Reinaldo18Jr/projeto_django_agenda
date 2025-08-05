b049f4069ba15f83a5cf8e12114f158f0aacd473
Reinaldo <reinaldo18junior@gmail.com>
2025-08-05 15:54:50 -0300

    UserCreationForm para criar novos usuários

--- contact/urls.py
+++ contact/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # user
+    path('user/create/', views.register, name='register'),
 ]
